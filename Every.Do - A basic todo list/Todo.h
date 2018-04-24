fbd447523c44f93f805ac3afadd1625d301876c2
Colin Russell <colinrussell101@gmail.com>
2018-04-24 10:23:18 -0400

    added ToDo class and UITableView with UITableViewCell

---
+++ Every.Do - A basic todo list/Todo.h
@@ -0,0 +1,16 @@
+//
+//  Todo.h
+//  Every.Do - A basic todo list
+//
+//  Created by Colin on 2018-04-24.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Todo : NSObject
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *todoDescription;
+@property (nonatomic) NSInteger priorityNumber;
+@property (nonatomic) BOOL isCompleted;
+@end
---
+++ Every.Do - A basic todo list/Todo.m
@@ -0,0 +1,22 @@
+//
+//  Todo.m
+//  Every.Do - A basic todo list
+//
+//  Created by Colin on 2018-04-24.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import "Todo.h"
+
+@implementation Todo
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
